9880ffd5ef0d0f62d30341b54acac7e036d7f6db
Pierre CHAPELOT <pierre.chapelot@gmail.com>
2023-04-17 14:24:30 +0200

    test

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app = require("express")();
 var http = require("http").createServer(app);
 const PORT = 8080;
 var io = require("socket.io")(http);
+const STATIC_CHANNELS = ["global_notifications", "global_chat"];
 
 http.listen(PORT, () => {
   console.log(`listening on *:${PORT}`);
@@ -15,5 +16,7 @@ io.on("connection", (socket) => {
   /* socket object may be used to send specific messages to the new connected client */
 
   console.log("new client connected");
+  
+  // customs event can be sent from the back end to the front end with this new socket connection
+  socket.emit("connection", null);
 });
-   
