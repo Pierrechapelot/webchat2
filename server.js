61f394041a75962f5a845d98e038a552af4c8ea4
Pierre CHAPELOT <pierre.chapelot@gmail.com>
2023-04-17 10:32:50 +0200

    first socket listener

---
+++ server.js
@@ -0,0 +1,16 @@
+// This is the nodeJS file that will execute all the logic of our application.
+
+var app = require('express')(); var http = require ('http').createServer(app); const PORT = 8080;
+
+http.listen(PORT, () => {
+
+    console.log(`listening on *:${PORT}`)
+});
+
+
+// We create our first socket listener
+
+io.on('connection', (socket) => { /* socket object may be used to send specific messages to the new connected client */
+
+    console.log('new client connected');
+});
